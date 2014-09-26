637d13ce41d86606610c64b4f237bb9fd6111ef1
babelberry <tatienantes@yahoo.fr>
2014-09-26 18:51:34 +0100

    changed port to help app deploy to heroku

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ server.get('/page3', function(request, response){
   response.send('<h1>The Sun is a terrible newspaper</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on port 3000');
+http.listen($PORT, function(){
+  console.log('listening on port' + $PORT);
 });
