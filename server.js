7ea7b74394d6bee05edd2d6aac09bb8b5bb61a5b
babelberry <tatienantes@yahoo.fr>
2014-09-26 13:26:08 +0100

    added the server in Node

---
+++ server.js
@@ -0,0 +1,20 @@
+var server = require('express')();
+var http = require('http').Server(server);
+//var ejs = require('ejs'); to be included if you're not using express
+server.set('views', __dirname + '/views');
+server.set('view engine', 'ejs');
+server.set('css', __dirname + '/css');
+server.use(require('express-ejs-layouts'));
+server.use(require('express').static('public'));
+
+server.get('/', function(request, response){
+  response.sendfile('index.html');
+});
+
+server.get('/page3', function(request, response){
+  response.send('<h1>The Sun is a terrible newspaper</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
